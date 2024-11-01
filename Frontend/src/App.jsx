d6b5f5c63882d3c839810ef9408357bb379fb3b9
Amit Sisodiya <amitsisodiyajadaun@gmail.com>
2024-11-01 18:19:46 +0530

    roadmap file add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element= {<div>Homepage</div>} />
         <Route path="/authentication" element={<AuthenticationPage />} />
 
         <Route path="/profile" element={<ProfilePage />} />
